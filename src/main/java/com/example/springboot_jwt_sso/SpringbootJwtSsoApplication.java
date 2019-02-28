a160df0a1cb9dd322c4b62f94899c9e1c58ed3df
haipeng.liu@hand-china.com <haipeng.liu@hand-china.com>
2019-02-28 16:20:01 +0800

    jwt验证sso机制

---
+++ src/main/java/com/example/springboot_jwt_sso/SpringbootJwtSsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtSsoApplication.class, args);
+	}
+
+}
